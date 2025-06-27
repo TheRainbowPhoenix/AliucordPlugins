d4550417c36c6b6793128ddb882413cb08b12260
PC <TheRainbowPhoenix@users.noreply.github.com>
2025-06-27 09:54:00 +0000

    remove older

--- PlayableEmbeds/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-version = "1.0.2"
-description = "Makes Spotify and Youtube Embeds playable"
-
-aliucord.changelog.set("""
-    # 1.0.2
-    * Youtube embed now supports timestamps
-""".trimIndent())
